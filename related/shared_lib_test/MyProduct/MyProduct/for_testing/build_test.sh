6b300f8ffcb608a49a376a32f6dded8541ee7af9
areiman <andrew.reiman@pnnl.gov>
2018-05-08 18:08:48 -0700

    compile wrapper with shared library linking to MATLAB runtime libraries instead of MATLAB installed libraries

--- related/shared_lib_test/MyProduct/MyProduct/for_testing/build_test.sh
+++ related/shared_lib_test/MyProduct/MyProduct/for_testing/build_test.sh
@@ -1,5 +1,20 @@
-rm shared_lib_test
-#mbuild MyProduct.cpp shared_lib_test.cpp
-mbuild shared_lib_test.cpp MyProduct.so
-#g++ -std=c++11 -I /usr/local/MATLAB/MATLAB_Runtime/v91/extern/include/ shared_lib_test.cpp -o shared_lib_test.out
+echo "Building"
+#rm shared_lib_test
 
+#mbuild MyProduct.cpp shared_lib_test.cpp
+#mbuild -v shared_lib_test.cpp MyProduct.so
+
+#g++ -std=c++11 -I /usr/local/MATLAB/MATLAB_Runtime/v91/extern/include/ shared_lib_test.cpp -o shared_lib_test.out -L /usr/local/MATLAB/MATLAB_Runtime/v91/extern/lib/glnxa64/
+
+mkdir /tmp/MyProduct/
+
+echo "Compiling..."
+g++ -std=c++11 -c -o /tmp/MyProduct/shared_lib_test.o -I/usr/local/MATLAB/MATLAB_Runtime/v91/extern/include/ shared_lib_test.cpp
+
+echo "Linking..."
+g++ -pthread -Wl,-rpath-link,/usr/local/MATLAB/MATLAB_Runtime/v91/bin/glnxa64 -O /tmp/MyProduct/shared_lib_test.o MyProduct.so /usr/local/MATLAB/MATLAB_Runtime/v91/sys/os/glnxa64/libstdc++.so.6 -L/usr/local/MATLAB/MATLAB_Runtime/v91/runtime/glnxa64 -lm -lmwmclmcrrt -o shared_lib_test.out
+
+echo "Cleaning up..."
+rm -rf /tmp/MyProduct/
+
+echo "Complete"
--- related/shared_lib_test/MyProduct/MyProduct/for_testing/run_test.sh
+++ related/shared_lib_test/MyProduct/MyProduct/for_testing/run_test.sh
@@ -1,2 +1,3 @@
 export LD_LIBRARY_PATH=.:/usr/local/MATLAB/MATLAB_Runtime/v91/runtime/glnxa64/:
-./shared_lib_test
+#./shared_lib_test
+./shared_lib_test.out
