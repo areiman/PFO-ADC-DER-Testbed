9517d58191a8fe990596995bbf2a2cd8d3b194db
Ankit Singhal <ankitsinghal1291@gmail.com>
2019-04-24 15:11:50 -0700

    1. added control timestep flexibility 2. saving Popt and Qopt as json file

--- ADC-DER-Testbed/testbed/Python_Wrapper/cosim_manager.py
+++ ADC-DER-Testbed/testbed/Python_Wrapper/cosim_manager.py
@@ -11,6 +11,9 @@ import json
 
 time_stop = int(sys.argv[1])
 time_granted = 0
+time_last = 0
+control_timestep = 4
+time_next = control_timestep
 op = open (sys.argv[2], "w")
 PQ_opt_dict={} #to store optimal PQ data from control modules
 
@@ -20,25 +23,29 @@ print('*****FNCS HAS INITIALIZED******')
 print("# time      key       value", file=op)
 
 while time_granted < time_stop:
-	time_granted = fncs.time_request(time_stop)
-	events = fncs.get_events()
-	SubKeys = []
-	SubKeyVals = []
-	for key in events:
-		print(time_granted, key.decode(), fncs.get_value(key).decode(), file=op)
-		Temp = str( key.decode())
-		SubKeys.append(Temp)
-		Temp=str( fncs.get_value(key).decode() )
-		SubKeyVals.append(Temp)
-	print("****SubKeys*****")
-	print(SubKeys)
-	keys,key_val, PQ_opt = fncs_parser.synch(SubKeys,SubKeyVals)
-	PQ_opt_dict[str(time_granted)] = PQ_opt
-	for i in range(len(keys)):
-		print(str(keys[i]))
-		print(str(key_val[i]))
-		fncs.publish(str(keys[i]), str(key_val[i]))
-	time.sleep(5)
+	time_granted = fncs.time_request(time_next)
+	time_delta = time_granted - time_last
+	time_last = time_granted
+	if time_granted == time_next:
+		time_next = time_next + control_timestep
+		events = fncs.get_events()
+		SubKeys = []
+		SubKeyVals = []
+		for key in events:
+			print(time_granted, key.decode(), fncs.get_value(key).decode(), file=op)
+			Temp = str( key.decode())
+			SubKeys.append(Temp)
+			Temp=str( fncs.get_value(key).decode() )
+			SubKeyVals.append(Temp)
+		print("****SubKeys*****")
+		print(SubKeys)
+		keys,key_val, PQ_opt = fncs_parser.synch(SubKeys,SubKeyVals)
+		PQ_opt_dict[str(time_granted)] = PQ_opt
+		for i in range(len(keys)):
+			print(str(keys[i]))
+			print(str(key_val[i]))
+			fncs.publish(str(keys[i]), str(key_val[i]))
+		time.sleep(5)
 
 with open('cosim_data.json', 'w') as fp:
     json.dump(PQ_opt_dict, fp, sort_keys=True, indent=4)
