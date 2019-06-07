5c9d91ca32dcf5ce1276a706a7dcbe3b44e71415
Ankit Singhal <ankitsinghal1291@gmail.com>
2019-06-07 10:24:44 -0700

    updating error_metrics.py

--- ADC-DER-Testbed/testbed/post_process/error_metrics.py
+++ ADC-DER-Testbed/testbed/post_process/error_metrics.py
@@ -17,28 +17,28 @@ def calculate(adc_agg, adc_Prating, cosim_data):
         adc_err_dict["adc_num"].append(adc_num)
         if "battInv" in adc_agg[adc_num]:
             num_der[adc_num] = len(adc_agg[adc_num]['battInv'])
-            temp_err = abs(cosim_data[adc_num]['batt_p'][0:2] - np.real(adc_agg[adc_num]['battInv'])[3:6:2])
+            temp_err = abs(cosim_data[adc_num]['Popt_BATT'][0:2] - np.real(adc_agg[adc_num]['battInv'])[3:6:2])
             adc_err_dict['battInv'] = np.append(adc_err_dict['battInv'], (np.sum(temp_err)/len(temp_err)))
         else:
             adc_err_dict['battInv'] = np.append(adc_err_dict['battInv'], np.nan)
 
         if "solarInv" in adc_agg[adc_num]:
             num_der[adc_num] = num_der[adc_num] + len(adc_agg[adc_num]['solarInv'])
-            temp_err = abs(cosim_data[adc_num]['pv_p'][0:2] - np.real(adc_agg[adc_num]['solarInv'])[3:6:2])
+            temp_err = abs(cosim_data[adc_num]['Popt_PV'][0:2] - np.real(adc_agg[adc_num]['solarInv'])[3:6:2])
             adc_err_dict['solarInv'] = np.append(adc_err_dict['solarInv'], (np.sum(temp_err)/len(temp_err)))
         else:
             adc_err_dict['solarInv'] = np.append(adc_err_dict['solarInv'], np.nan)
 
         if "hvac" in adc_agg[adc_num]:
             num_der[adc_num] = num_der[adc_num] + len(adc_agg[adc_num]['hvac'])
-            temp_err = abs(cosim_data[adc_num]['ac_p'][0:2] - np.real(adc_agg[adc_num]['hvac'])[3:6:2])
+            temp_err = abs(cosim_data[adc_num]['Popt_HVAC'][0:2] - np.real(adc_agg[adc_num]['hvac'])[3:6:2])
             adc_err_dict['hvac'] = np.append(adc_err_dict['hvac'], (np.sum(temp_err)/len(temp_err)))
         else:
             adc_err_dict['hvac'] = np.append(adc_err_dict['hvac'], np.nan)
 
         if "wh" in adc_agg[adc_num]:
             num_der[adc_num] = num_der[adc_num] + len(adc_agg[adc_num]['wh'])
-            temp_err = abs(cosim_data[adc_num]['ewh_p'][0:2] - np.real(adc_agg[adc_num]['wh'])[3:6:2])
+            temp_err = abs(cosim_data[adc_num]['Popt_WH'][0:2] - np.real(adc_agg[adc_num]['wh'])[3:6:2])
             adc_err_dict['wh'] = np.append(adc_err_dict['wh'], (np.sum(temp_err)/len(temp_err)))
         else:
             adc_err_dict['wh'] = np.append(adc_err_dict['wh'], np.nan)
