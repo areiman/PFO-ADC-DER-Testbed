49d10eaedd0ffeb4de2d2d91762748447d01d36f
Ankit Singhal <ankitsinghal1291@gmail.com>
2019-07-05 11:13:26 -0700

    updated runall

--- ADC-DER-Testbed/testbed/run/runall.sh
+++ ADC-DER-Testbed/testbed/run/runall.sh
@@ -1,12 +1,10 @@
-#!/bin/bash 
- 
-clear 
+#!/usr/bin/env bash
 
 cd ~/PFO-ADC-DER-Testbed/ADC-DER-Testbed/testbed/run && export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=INFO && export FNCS_TRACE=yes && export FNCS_BROKER=tcp://*:5570 && exec fncs_broker 2 &> broker.log &
 
 cd ~/PFO-ADC-DER-Testbed/ADC-DER-Testbed/testbed/GLD/ && export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=INFO && export FNCS_BROKER=tcp://localhost:5570 && exec gridlabd IEEE_123_feeder_0_DER_demo-recorders_Rev0.glm &> glm.log &
 
-cd ~/PFO-ADC-DER-Testbed/ADC-DER-Testbed/testbed/Python_Wrapper/ && export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=INFO && export FNCS_BROKER=tcp://localhost:5570 && export FNCS_CONFIG_FILE=fncs.zpl && exec python3.6 cosim_manager.py 9 cosim_manager.txt &> cosim_manager.log &
+cd ~/PFO-ADC-DER-Testbed/ADC-DER-Testbed/testbed/Python_Wrapper/ && export FNCS_LOG_STDOUT=yes && export FNCS_LOG_LEVEL=INFO && export FNCS_BROKER=tcp://localhost:5570 && export FNCS_CONFIG_FILE=fncs.zpl && exec python3.6 cosim_manager.py 1510 cosim_manager.txt &> cosim_manager.log &
 
 wait 
  
