a34931133ea5079969d3004c0587792a3d23ea06
Ankit Singhal <ankitsinghal1291@gmail.com>
2019-07-26 13:30:22 -0700

    minor changes to support multipe gld instances

--- ADC-DER-Testbed/testbed/post_process/post_process.py
+++ ADC-DER-Testbed/testbed/post_process/post_process.py
@@ -6,67 +6,76 @@ import csv;
 import glob
 import os
 
+print("----Make sure start_time is set correctly in post_process.py----")
+start_time = '2013-07-29 12:00:00 PST'
+
+start_record = 0
 cwd = os.getcwd()
 os.chdir('/home/ankit/PFO-ADC-DER-Testbed/ADC-DER-Testbed/testbed/post_process')
-
+GLD_folders = ['GLD_1']
 ## Readinf gld csv files
-csv_path = './../GLD/output/recorder_*.csv'
-skip_filenames = ['./../GLD/output/recorder_wh_heat_mode.csv',
-                  './../GLD/output/recorder_hvac_cooling_system_type.csv',
-                  './../GLD/output/recorder_hvac_auxiliary_system_type.csv',
-                  './../GLD/output/recorder_battInv_max_charge_rate.csv',
-                  './../GLD/output/recorder_battInv_inverter_efficiency.csv',
-                  './../GLD/output/recorder_batt_battery_capacity.csv',
-                  './../GLD/output/recorder_hvac_heating_system_type.csv']
-# region Reading csv into a dictionary
-temp_dict1={}
-gld_data = {}
-gld_data['batt'] = {}
-gld_data['battInv'] = {}
-gld_data['hvac'] = {}
-gld_data['wh'] = {}
-gld_data['solar'] = {}
-gld_data['solarInv'] = {}
+for gld in GLD_folders:
+    csv_path = './../'+gld+'/output/recorder_*.csv'
+    skip_filenames = ['./../'+gld+'/output/recorder_wh_heat_mode.csv',
+                      './../'+gld+'/output/recorder_hvac_cooling_system_type.csv',
+                      './../'+gld+'/output/recorder_hvac_auxiliary_system_type.csv',
+                      './../'+gld+'/output/recorder_battInv_max_charge_rate.csv',
+                      './../'+gld+'/output/recorder_battInv_inverter_efficiency.csv',
+                      './../'+gld+'/output/recorder_batt_battery_capacity.csv',
+                      './../'+gld+'/output/recorder_hvac_heating_system_type.csv']
+    # region Reading csv into a dictionary
+    temp_dict1={}
+    gld_data = {}
+    gld_data['batt'] = {}
+    gld_data['battInv'] = {}
+    gld_data['hvac'] = {}
+    gld_data['wh'] = {}
+    gld_data['solar'] = {}
+    gld_data['solarInv'] = {}
 
-
-for fname in glob.glob(csv_path):
-    if fname in skip_filenames:
-        continue
-    print(fname)
-    splt = fname.split('recorder_')[1]
-    DER_type = splt.split('_',1)[0]
-    property = splt.split('_',1)[1][0:-4]
-    values = []
-    time =[]
-    temp_dict = {}
-    with open(fname, mode='r') as csv_file:
-        for i in range(8):
-            next(csv_file)
-        csv_reader = csv.reader(csv_file)
-        header_line = 1
-        for row in csv_reader:
-            if row[0] == '# end of file':
-                break
-            if header_line:
-                temp_dict['object_name'] = row[1:]
-                header_line = 0
+    for fname in glob.glob(csv_path):
+        if fname in skip_filenames:
+            continue
+        print(fname)
+        splt = fname.split('recorder_')[1]
+        DER_type = splt.split('_',1)[0]
+        property = splt.split('_',1)[1][0:-4]
+        values = []
+        time =[]
+        temp_dict = {}
+        with open(fname, mode='r') as csv_file:
+            start_record = 0
+            for i in range(8):
+                next(csv_file)
+            csv_reader = csv.reader(csv_file)
+            header_line = 1
+            # row_count = sum(1 for row in csv_reader)
+            for row in csv_reader:
+                if row[0] == '2013-07-29 17:09:56 PST':#'# end of file':
+                    break
+                if header_line:
+                    temp_dict['object_name'] = row[1:]
+                    header_line = 0
+                else:
+                    if row[0] == start_time: #or row_count == 3:
+                        start_record = 1
+                    if start_record == 1:
+                        time.append(row[0])
+                        values.append(row[1:])
+            if len(values) > 1 and len(values[-1]) != len(values[-2]):
+                values.pop()
+            values= np.array(values)
+            if 'i' in values[0][0] or 'j' in values[0][0]:
+                values = [[(ss.replace('i', 'j')) for ss in s] for s in values]
+                #values = list(map(cfloat, values))
             else:
-                time.append(row[0])
-                values.append(row[1:])
-        if len(values) > 1 and len(values[-1]) != len(values[-2]):
-            values.pop()
-        values= np.array(values)
-        if 'i' in values[0][0] or 'j' in values[0][0]:
-            values = [[(ss.replace('i', 'j')) for ss in s] for s in values]
-            #values = list(map(cfloat, values))
-        else:
-            values = [[float(ss) for ss in s] for s in values]
-        temp_dict['values'] = values
-        temp_dict['time'] = time
-    gld_data[DER_type][property] = temp_dict
+                values = [[float(ss) for ss in s] for s in values]
+            temp_dict['values'] = values
+            temp_dict['time'] = time
+        gld_data[DER_type][property] = temp_dict
 
-with open('./gld_data.json', 'w') as fp:
-    json.dump(gld_data, fp, sort_keys=True, indent=4)
+    with open('./'+gld+'_data.json', 'w') as fp:
+        json.dump(gld_data, fp, sort_keys=True, indent=4)
 # endregion
 
 # Reading cosim_data csv files from cosim_dat folder
@@ -78,8 +87,9 @@ for fname in glob.glob(csv_path):
     if fname in skip_filenames:
         continue
     print(fname)
-    splt = fname.split('M1_')[1]
-    adc_num = splt.split('.')[0][3:]
+    # splt = fname.split('M1_')[1]
+    splt = fname.split('cosim_dat/')[1]
+    adc_num = splt.split('.')[0]
     if adc_num not in cosim_data:
         cosim_data[adc_num] = {}
         # PQ_opt[adc_num]['Popt'] = []
