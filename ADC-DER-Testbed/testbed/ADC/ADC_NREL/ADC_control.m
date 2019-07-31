3399a40a45aaab69ffa67ba21f608eb92c99dde3
areiman <andrew.reiman@pnnl.gov>
2019-07-31 13:52:41 -0700

    updated ADC_control.m with version that accounts for the possibility of zero PV systems

--- ADC-DER-Testbed/testbed/ADC/ADC_NREL/ADC_control.m
+++ ADC-DER-Testbed/testbed/ADC/ADC_NREL/ADC_control.m
@@ -15,7 +15,7 @@ SOC_now;
 %class(cap_ba)
 %cap_ba
 
-max_iter=50000;
+max_iter=5000;
 
 % inverter output initialization
 p_pv = p_av;
@@ -47,53 +47,46 @@ cost_q = 1 * ones(n_pv,1);
 % power-soc convert rate = time/60/battery_capacity
 power_SOC_rate = deltat/60./cap_ba;
 % C_i^b=cost_bat*(SoC-0.5)^2
-cost_bat = deltat*1./power_SOC_rate;
+cost_bat = 100*deltat*1./power_SOC_rate;
 cost_bat_q = 1;
 
 
 for k=1:max_iter
     % primal update
-    for i=1:n_pv
-%        p_pv(i,k+1) = p_pv(i,k) - epsilonP * (2 * cost_p(i) * (-p_av(i) + p_pv(i,k)) + mup(k));
-%        q_pv(i,k+1) = q_pv(i,k) - epsilonP * (2 * cost_q(i) * q_pv(i,k) + muq(k));
-%        [p_pv(i,k+1), q_pv(i,k+1)] = Proj_inverter( p_pv(i,k+1), q_pv(i,k+1), p_av(i), cap_pv(i));
-        p_pv(i) = p_pv(i) - epsilonP * (2 * cost_p(i) * (-p_av(i) + p_pv(i)) + mup);
-        q_pv(i) = q_pv(i) - epsilonP * (2 * cost_q(i) * q_pv(i) + muq);
-        [p_pv(i), q_pv(i)] = Proj_inverter( p_pv(i), q_pv(i), p_av(i), cap_pv(i));
+    if n_pv >= 1
+        for i=1:n_pv
+    %        p_pv(i,k+1) = p_pv(i,k) - epsilonP * (2 * cost_p(i) * (-p_av(i) + p_pv(i,k)) + mup(k));
+    %        q_pv(i,k+1) = q_pv(i,k) - epsilonP * (2 * cost_q(i) * q_pv(i,k) + muq(k));
+    %        [p_pv(i,k+1), q_pv(i,k+1)] = Proj_inverter( p_pv(i,k+1), q_pv(i,k+1), p_av(i), cap_pv(i));
+            p_pv(i) = p_pv(i) - epsilonP * (2 * cost_p(i) * (-p_av(i) + p_pv(i)) + mup);
+            q_pv(i) = q_pv(i) - epsilonP * (2 * cost_q(i) * q_pv(i) + muq);
+            [p_pv(i), q_pv(i)] = Proj_inverter( p_pv(i), q_pv(i), p_av(i), cap_pv(i));
+        end
     end
-    for i=1:n_ba
-%         p_ba(i,k+1) = p_ba(i,k) -  epsilonP * ( - 2 * eff_ba(i) * power_SOC_rate(i)...
-%             * cost_bat(i) * (soc(i) - SOC_set(i) + power_SOC_rate(i)*eff_ba(i)*p_ba(i,k)) + mup(k));
-        gradient_ba = - 2 * eff_ba(i) * power_SOC_rate(i) ...
-            * cost_bat(i) * (soc(i) - SOC_set(i) + power_SOC_rate(i)*eff_ba(i));
-        p_ba(i) = p_ba(i) -  epsilonP * ( gradient_ba * p_ba(i) + mup);
-        q_ba(i) = q_ba(i) - epsilonP * ( cost_bat_q * q_ba(i) + muq);
+    if n_ba >= 1
+        for i=1:n_ba
+    %         p_ba(i,k+1) = p_ba(i,k) -  epsilonP * ( - 2 * eff_ba(i) * power_SOC_rate(i)...
+    %             * cost_bat(i) * (soc(i) - SOC_set(i) + power_SOC_rate(i)*eff_ba(i)*p_ba(i,k)) + mup(k));
+            gradient_ba = - 2 * eff_ba(i) * power_SOC_rate(i) ...
+                * cost_bat(i) * (soc(i) - SOC_set(i) + power_SOC_rate(i)*eff_ba(i)*p_ba(i));
+    % gradient_ba = 2;
+            p_ba(i) = p_ba(i) -  epsilonP * ( gradient_ba * p_ba(i) + mup);
+            q_ba(i) = q_ba(i) - epsilonP * ( cost_bat_q * q_ba(i) + muq);
 
-%         % Battery rates projection
-%         if p_ba(i,k+1) > p_ba_dis_max(i)
-%             p_ba(i,k+1) = p_ba_dis_max(i);
-%         elseif p_ba(i,k+1) < p_ba_cha_max(i)
-%             p_ba(i,k+1) = p_ba_cha_max(i);
-%         end
-        % Battery rates projection
-
-        [p_ba(i), q_ba(i)] = Proj_ba_inverter( p_ba(i), q_ba(i), p_ba_dis_max(i), p_ba_cha_max(i), cap_ba_inverter(i));
-
-
-%         if p_ba(i) > p_ba_dis_max(i)
-%             p_ba(i) = p_ba_dis_max(i);
-%         elseif p_ba(i) < p_ba_cha_max(i)
-%             p_ba(i) = p_ba_cha_max(i);
-%         end
+            [p_ba(i), q_ba(i)] = Proj_ba_inverter( p_ba(i), q_ba(i), p_ba_dis_max(i), p_ba_cha_max(i), cap_ba_inverter(i));
+        end
     end
 
     % dual update
-%     mup(k+1) = mup(k) + epsilonD * (sum(p_pv(:,k)) + sum(p_ba(:,k)) - p_opt);
-%     muq(k+1) = muq(k) + epsilonD * (sum(q_pv(:,k)) - q_opt);
     mup = mup + epsilonD * (sum(p_pv) + sum(p_ba) - p_opt);
     muq = muq + epsilonD * (sum(q_pv) + sum(q_ba) - q_opt);
 
+   % record_mu(k) = mup;
+
 end
+
+
+%plot(record_mu);
 
 % mup = mup
 % muq = muq
